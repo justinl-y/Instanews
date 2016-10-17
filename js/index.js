75bf60b9ce8ec7bfc6a505e558d825c2ae1feb3d
justinl-y <j@solaris.org.uk>
2016-10-17 12:11:41 -0700

    Heapbox jquery plugin added

--- js/index.js
+++ js/index.js
@@ -2,114 +2,102 @@
 
 $(document).ready(function() {
     var $loadingImage = $('.loadingImage');
-
     $loadingImage.hide();
 
-    $("#sections-form").on("change", "select", function (e) {
-        e.preventDefault();
+    $("#sections").heapbox({
+        "onChange": function(val,sEl){
+            var sectionName = val;
+            var $newsItemsList = $(".newsItems");
 
-        //var selectValue = $(e.target).val();
-        //var selectText = $(e.target).find('option:selected').text(); //only time the find is required
-        //var selectName = $(e.target).attr('name');
-
-        //console.log(selectValue);
-        //console.log(selectText);
-        //console.log(selectName);
-
-        var sectionName = $(e.target).val();
-        var $newsItemsList = $(".newsItems");
-
-        if (sectionName == "0") {
-            //remove data on default value
-            //$newsItemsList.empty();
-            $newsItemsList.empty().append("<p>Sorry. No news items found.</p>");
-        } else {
-            $loadingImage.show();
-
-            //set up url to connect to nyt
-            var url = "https://api.nytimes.com/svc/topstories/v2/" + sectionName + ".json";
-            url += "?" + $.param({
-                    "api-key": "e969eed331bd4fc8a6764edfb463db8b"
-                });
-
-            //begin ajax call
-            $.ajax({
-                url: url,
-                method: "GET"
-            }).done(function(result) {
-
-                if (result.num_results === 0) {
-                    //if no results
-                    $newsItemsList.empty().append("<p>Sorry. No news items found.</p>");
-                }  else {
-
-                    //call modify css function on screen size
-                    $(window).width(function() {
-                        $.fn.changeCSSOnScreenResolution();
+            if (sectionName == "0") {
+                //remove data on default value
+                $newsItemsList.empty().append("<p>Sorry. No news items found.</p>");
+            } else {
+                //set up url to connect to nyt
+                var url = "https://api.nytimes.com/svc/topstories/v2/" + sectionName + ".json";
+                url += "?" + $.param({
+                        "api-key": "e969eed331bd4fc8a6764edfb463db8b"
                     });
 
-                    var newsItemsList = result.results;
-                    var newsItems = [];
-                    var newsItem = {};
+                //begin ajax call
+                $.ajax({
+                    url: url,
+                    method: "GET"
+                }).done(function(result) {
 
-                    //extract news items from news listing json
-                    $.each(newsItemsList, function (key) {
-                        if (newsItemsList[key].multimedia.length !== 0) {
-                            newsItem = {abstract: newsItemsList[key].abstract,
-                                image: newsItemsList[key].multimedia[4].url,
-                                url: newsItemsList[key].url,
-                                urlTitle: newsItemsList[key].title};
+                    if (result.num_results === 0) {
+                        //if no results
+                        $newsItemsList.empty().append("<p>Sorry. No news items found.</p>");
+                    }  else {
 
-                            newsItems.push(newsItem);
+                        //call modify css function on screen size
+                        $(window).width(function() {
+                            $.fn.changeCSSOnScreenResolution();
+                        });
+
+                        var newsItemsList = result.results;
+                        var newsItems = [];
+                        var newsItem = {};
+
+                        //extract news items from news listing json
+                        $.each(newsItemsList, function (key) {
+                            if (newsItemsList[key].multimedia.length !== 0) {
+                                newsItem = {abstract: newsItemsList[key].abstract,
+                                    image: newsItemsList[key].multimedia[4].url,
+                                    url: newsItemsList[key].url,
+                                    urlTitle: newsItemsList[key].title};
+
+                                newsItems.push(newsItem);
+                            }
+                        });
+
+                        var newsItemsMarkup = '';
+
+                        //get max 12 items from newsItems
+                        for (var i = 0; i < 12; i++) {
+                            //create span of abstract text
+                            var newsAbstract = '<span class="newsItemTextFormatting">' + newsItems[i].abstract + '</span>';
+
+                            //create news item div id number for adding image background
+                            var newsItemID = "newsItem" + (i + 1).toString();
+
+                            //create news image div and add span of news abstract
+                            var newsItemInnerMarkup = '<div class="newsImage" id="' + newsItemID + '">' + newsAbstract + '</div>';
+
+                            //create a href to containing news url and child divs
+                            var newsItemOuterMarkup = '<a href="' +
+                                newsItems[i].url +
+                                '" class="newsItem" target="_blank" title="' + newsItems[i].urlTitle + '">' +
+                                newsItemInnerMarkup +
+                                '</a>';
+
+                            newsItemsMarkup += newsItemOuterMarkup;
                         }
-                    });
 
-                    var newsItemsMarkup = '';
+                        //append block of markup to index.html
+                        $newsItemsList.empty().append(newsItemsMarkup);
 
-                    //get max 12 items from newsItems
-                    for (var i = 0; i < 12; i++) {
-                        //create span of abstract text
-                        var newsAbstract = '<span class="newsItemTextFormatting">' + newsItems[i].abstract + '</span>';
+                        //change css for appended markup
+                        for (var j = 0; j < 12; j++) {
+                            var newsItemID2 = "#newsItem" + (j + 1).toString();
 
-                        //create news item div id number for adding image background
-                        var newsItemID = "newsItem" + (i + 1).toString();
+                            var newsImageURL = "url(" + newsItems[j].image + ")";
 
-                        //create news image div and add span of news abstract
-                        var newsItemInnerMarkup = '<div class="newsImage" id="' + newsItemID + '">' + newsAbstract + '</div>';
+                            var $newsImage = $(newsItemID2).css("background-image", newsImageURL);
 
-                        //create a href to containing news url and child divs
-                        var newsItemOuterMarkup = '<a href="' +
-                                                        newsItems[i].url +
-                                                        '" class="newsItem" target="_blank" title="' + newsItems[i].urlTitle + '">' +
-                                                        newsItemInnerMarkup +
-                                                        '</a>';
+                            //to do add css modify for whole background property
+                            //url("../images/product-categories/sale-category.jpg") no-repeat center center
+                        }
 
-                        newsItemsMarkup += newsItemOuterMarkup;
                     }
+                }).fail(function(err) {
+                    //throw err;
 
-                    //append block of markup to index.html
-                    $newsItemsList.empty().append(newsItemsMarkup);
-
-                    //change css for appended markup
-                    for (var j = 0; j < 12; j++) {
-                        var newsItemID2 = "#newsItem" + (j + 1).toString();
-
-                        var newsImageURL = "url(" + newsItems[j].image + ")";
-
-                        var $newsImage = $(newsItemID2).css("background-image", newsImageURL);
-
-                        //to do add css modify for whole background property
-                        //url("../images/product-categories/sale-category.jpg") no-repeat center center
-                    }
-
-                }
-            }).fail(function(err) {
-                //throw err;
-
-                $newsItemsList.empty().append('<p>Sorry. An error has occured.</p>');
-            }).always(function() {
-                $loadingImage.hide();
-            });
+                    $newsItemsList.empty().append("<p>Sorry. An error has occured.</p>");
+                }).always(function() {
+                    $loadingImage.hide();
+                });
+            }
         }
     });
 
@@ -131,14 +119,14 @@ $(document).ready(function() {
             if (!(currentScreenSize === existingScreenSize)) {
                 existingScreenSize = currentScreenSize;
                 $("header").css({
-                    "padding-top": "2rem",
-                    "padding-bottom": "1rem",
-                    "height": "auto"
+                    "padding-top" : "2rem",
+                    "padding-bottom" : "1rem",
+                    "height" : "auto"
                 });
 
                 $(".logo").css({
-                    "height": "150px",
-                    "width": "auto",
+                    "height" : "150px",
+                    "width" : "auto",
                     "padding-bottom": "1rem"
                 });
 
@@ -154,15 +142,15 @@ $(document).ready(function() {
                 existingScreenSize = currentScreenSize;
 
                 $("header").css({
-                    "padding-top": "2rem",
-                    "padding-bottom": "1rem",
-                    "height": "auto"
+                    "padding-top" : "2rem",
+                    "padding-bottom" : "1rem",
+                    "height" : "auto"
                 });
 
                 $(".logo").css({
-                    "height": "75px",
-                    "width": "auto",
-                    "padding-bottom": "1rem"
+                    "height" : "75px",
+                    "width" : "auto",
+                    "padding-bottom" : "1rem"
                 });
 
                 $("#sections-form").css({"margin-left" : "2rem"});
@@ -174,15 +162,15 @@ $(document).ready(function() {
             if (!(currentScreenSize === existingScreenSize)) {
                 existingScreenSize = currentScreenSize;
                 $("header").css({
-                    "padding-top": "2rem",
-                    "padding-bottom": "1rem",
-                    "height": "auto"
+                    "padding-top" : "2rem",
+                    "padding-bottom" : "1rem",
+                    "height" : "auto"
                 });
 
                 $(".logo").css({
-                    "height": "75px",
-                    "width": "auto",
-                    "padding-bottom": "1rem"
+                    "height" : "75px",
+                    "width" : "auto",
+                    "padding-bottom" : "1rem"
                 });
 
                 $("#sections-form").css({"margin-left" : "2rem"});
